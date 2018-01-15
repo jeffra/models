8d48779ecf9335c1173e6697735c4303952b5d43
Jeff Rasley <jeffra@cs.brown.edu>
2018-01-15 11:21:13 -0500

    add alert to end of training

--- tutorials/rnn/ptb/train_model.sh
+++ tutorials/rnn/ptb/train_model.sh
@@ -4,3 +4,5 @@ model=${1:-"small"}
 echo "Training $model PTB model"
 
 python -u ptb_word_lm.py $@ &> ptb_${model}.log
+
+curl "https://api.simplepush.io/send/4B4a4N/ExperimentDone/$model $(hostname)"
